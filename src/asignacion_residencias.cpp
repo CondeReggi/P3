6fb919e4321d9398235c3f3695387bd4d16ca06d
CondeReggi <srtoxing@gmail.com>
2022-09-06 11:58:57 -0300

    finished

--- src/asignacion_residencias.cpp
+++ src/asignacion_residencias.cpp
@@ -1,73 +1,86 @@
-/*
-  Módulo de implementación de `asignacion_residencias'.
-
-  Laboratorio de Programación 3.
-  InCo-FIng-UDELAR
- */
-
 #include "../include/asignacion_residencias.h"
 #include "../include/asignacion.h"
 #include "../include/pila.h"
 #include "../include/utils.h"
 #include <cstddef>
+#include <stdio.h>
 
-
-//Proponen los hospitales
-Asignacion asignarResidencias(nat m, nat* C, nat n, nat** hPrefs, nat** ePrefs)
+// Proponen los hospitales
+Asignacion asignarResidencias(nat m, nat *C, nat n, nat **hPrefs, nat **ePrefs)
 {
-    Pila /* Gordon */ freeHospital = crear_pila();
-    Asignacion result = crear_asignacion(); //Creo result para posteriormente llenarlo con matches
-    nat current; 
-    nat current_student;
-    nat* next = new nat[m]; 
-    nat* matches = new nat[n];
-    nat** ranking = new nat*[n];
+  Pila /* Gordon */ freeHospital = crear_pila();
+  Asignacion result = crear_asignacion(); // Creo result para posteriormente llenarlo con matches
+  nat current;
+  nat current_student;
+  int *next = new int[m];
+  int *matches = new int[n];
+  int **ranking = new int *[n];
 
-    for(nat i = 0; i < n; i++){ 
-      for(nat j = 0; j < n; j++){ 
-        ranking[i][ePrefs[i][j]] = j;
-      }
+  for (nat i = 0; i < n; i++)
+  {
+    ranking[i] = new int[n];
+    for (nat j = 0; j < m; j++)
+    {
+      ranking[i][ePrefs[i][j]] = j;
+    }
+  }
+
+  for (nat i = 0; i < m; i++)
+  { // Pongo de 1 a M no de 0 a M-1
+    apilar(i, freeHospital);
+    next[i] = 0;
+  }
+  for (nat i = 0; i < n; i++)
+  {
+    matches[i] = -1;
+  }
+  int sum = 1;
+  current = cima(freeHospital);
+  while (!es_vacia_pila(freeHospital) && next[current] < (int)n)
+  {
+    sum = sum + 1;
+    current = cima(freeHospital);
+
+    if(C[current] == 0){
+      desapilar(freeHospital);
+      continue;
     }
 
-    for (nat i = 1; i <= m; i++) { //Pongo de 1 a M no de 0 a M-1
-      apilar(i, freeHospital);
-      next[i] = 1;
+    current_student = hPrefs[current][next[current]];
+    next[current] = next[current] + 1;
+    if (matches[current_student] == -1)
+    {
+      matches[current_student] = current;
+      C[current] = C[current] - 1;
     }
-    for(nat i = 0; i < n; i++){
-      matches[i] = 0;
+    else if (ranking[current_student][current] < ranking[current_student][matches[current_student]])
+    { // Aca el ranking tiene mayor prioruidad el estudiante de matches[current_student]
+      apilar(matches[current_student], freeHospital);
+      C[matches[current_student]] = C[matches[current_student]] + 1;
+      matches[current_student] = current;
+      C[current] = C[current] - 1;
     }
+  }
 
-    while(!es_vacia_pila(freeHospital)){
-      current = cima(freeHospital);
-      current_student = hPrefs[current][next[current]];
-      next[current]++;
+  for (int i = 0; i < (int)n; i++)
+  { // Recorro y voy asignando, se supone en maches esta todo
+    par *nuevo = new par;
+    nuevo->eid = i;
+    nuevo->hid = matches[i];
+    insertar_par(*nuevo, result);
+    delete (nuevo);
+  }
 
-      if(matches[current_student] == 0){
-        matches[current_student] = current;
-        desapilar(freeHospital);
+  destruir_pila(freeHospital); // Elimino la cola
 
-      }else if(ranking[current][current_student] < ranking[current][matches[current_student]]){
-        desapilar(freeHospital);
-        apilar(matches[current_student], freeHospital);
-        matches[current_student] = current;
-      }
-    }
+  for (nat i = 0; i < n; i++)
+  {
+    delete[] ranking[i]; // Elimino todos antes
+  }
 
-    for(int i = 0; i < n; i++){ //Recorro y voy asignando, se supone en maches esta todo
-      par *nuevo = new par;
-      nuevo->eid = i;
-      nuevo->hid = matches[i] - 1;
-      insertar_par(*nuevo, result);
-      delete(nuevo);
-    }
+  delete[] ranking; // Elimino el array de ranking
+  delete[] matches; // Elimino el array de matches
+  delete[] next;    // Elimino el array de next
 
-    destruir_pila(freeHospital); //Elimino la cola
-
-    delete(ranking); //Elimino el array de ranking
-    delete(matches); //Elimino el array de matches
-    delete(next); //Elimino el array de next
-    
-    return result; // se debe retornar algo de tipo asignacion
+  return result; // se debe retornar algo de tipo asignacion
 }
-
-
